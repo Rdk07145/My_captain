18cab15a0b23093c931835f59777ef9fbe4cdee3
Rdk07145 <86289471+Rdk07145@users.noreply.github.com>
2021-06-22 15:12:42 +0530

    Created Grading_marks.c
    This will grade the marks provided by the user

---
+++ Grading_marks.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+ int main() 
+ {
+   int m;
+   printf("Enter student's marks: ");
+   scanf("%d", &m);
+
+   if (m>=85)
+   {
+     printf("Grade A");
+   }
+   
+   
+    else if  (m>=70)
+   {
+    
+     printf("Grade B");
+   }
+
+   
+   else if (m>=55)
+   {
+     printf("Grade C");
+   }
+
+   
+   else if (m>=40)
+   {
+     printf("Grade D");
+   }
+
+   
+   else
+   {
+     printf("Grade F");
+   }
+   return 0;
+
+ }
