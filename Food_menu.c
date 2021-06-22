bf4509dda862da7c27ed8b26a15b8b489dd092eb
Rdk07145 <86289471+Rdk07145@users.noreply.github.com>
2021-06-22 16:03:34 +0530

    Created Food_menu.c
    Will show a surprise food item when a no. from 1 to 5 is inputted by the user

---
+++ Food_menu.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main() {
+
+  int menu = 0;
+  
+  printf("\n\nEnter a number from 1 to 5 to get a surprise food item : ");
+  scanf("%d",&menu);
+
+  switch(menu)
+  {
+    case 1:
+    printf("\n Food item - Burger\n Price - Rs 239/-");
+    break;
+
+     case 2:
+    printf("\n Food item - Pizza\n Price - Rs 129/-");
+    break;
+
+    case 3:
+    printf("\n Food item - Sandwich\n Price - Rs 179/-");
+    break;
+
+     case 4:
+    printf("\n Food item - Coca Cola\n Price - Rs 99/-");
+    break;
+
+     case 5:
+    printf("\n Food item - Pasta\n Price - Rs 149/-");
+    break;
+
+    default:
+    printf("\n ERROR (Enter a no. from 1 to 6)");
+    
+
+  }
+
+  return 0;
+}
