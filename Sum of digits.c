9262251251dfabd10fecce9b148e11b00940022f
Rohan deep Kujur <86289471+Rdk07145@users.noreply.github.com>
2021-06-27 16:31:33 +0530

    Created sum of digits.c
    This will show the sum of the digits of the number inputted by the user

---
+++ Sum of digits.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+ 
+// Function to check sum of digit using recursion
+int sum_of_digit(int n)
+{
+    if (n == 0)
+       return 0;
+    return (n % 10 + sum_of_digit(n / 10));
+}
+ 
+int main()
+{
+    int num ;
+    printf("Input no.: ");
+    scanf("%d",&num);
+
+    int result = sum_of_digit(num);
+    printf("Sum of digits: %d\n", result);
+    return 0;
+}
